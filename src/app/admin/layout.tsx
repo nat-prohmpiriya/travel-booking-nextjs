c3ce549bfdd3c0a30cc7574664482d6cada2308b
Prohmpiriya Phonunumnuaisuk <nut.prohmpiriya@gmail.com>
2025-07-13 17:23:49 +0700

    feat: implement admin dashboard with statistics, charts, and recent bookings

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,17 @@
+"use client";
 import AdminLayout from '@/components/admin-layout';
+import { usePathname } from 'next/navigation';
 
-export default function AdminLayoutWrapper({
-    children,
-}: {
+
+interface AdminLayoutWrapperProps {
     children: React.ReactNode;
-}) {
+}
+
+export default function AdminLayoutWrapper({ children }: AdminLayoutWrapperProps) {
+    // path === /admin no layout
+    const pathname = usePathname();
+    if (pathname === '/admin') {
+        return <>{children}</>;
+    }
     return <AdminLayout>{children}</AdminLayout>;
 }
